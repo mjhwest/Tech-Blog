d81a158550673afc9a5bce55cd7abdd7722852c3
mjhwest89 <mjhwest89@gmail.com>
2021-12-08 00:52:05 +1030

    index.js for routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
+//userRoutes 
 const userRoutes = require('./userRoutes');
+//postRoutes 
 const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
+
+//export router. 
 module.exports = router;
