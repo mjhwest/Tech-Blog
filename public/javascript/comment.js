b29b877bff591e62e51608daa8cc32f88f267b16
mjhwest89 <mjhwest89@gmail.com>
2021-12-13 14:48:17 +1030

    pushing to save

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -27,4 +27,4 @@ async function commentFormHandler(event) {
     }
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('.comments').addEventListener('submit', commentFormHandler);
